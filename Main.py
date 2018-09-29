39080255d52495b801025cda6e326970a7a837f6
Aertex <Aertex@users.noreply.github.com>
2018-09-28 18:58:36 -0700

    Started opencv implementaiton

--- Main.py
+++ Main.py
@@ -1,1 +1,10 @@
-print("Hello World!")
+import cv2
+import numpy
+
+capture = cv2.VideoCapture(0)
+
+while capture.isOpened():
+    ret, frame = capture.read()
+    cv2.imshow('input', frame)
+    k = cv2.waitKey(10)
+
