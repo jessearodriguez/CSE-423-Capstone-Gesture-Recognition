0c02330c4ee9b0958174168cd3052e2a2e20d1b2
Fatkitties <33295966+Fatkitties@users.noreply.github.com>
2018-10-10 07:54:42 -0700

    added test sound module

---
+++ soundtest.py
@@ -0,0 +1,16 @@
+from sound import Sound
+import time
+#Test file for volume test, using existing Sound Module
+
+#Note first call from the sound module will max out volume
+#Compensate by setting volume
+#Currently trying to create a new sound module
+
+Sound.volume_down()
+Sound.volume_set(20)
+time.sleep(2)
+Sound.volume_min()
+time.sleep(2)
+Sound.volume_set(40)
+
+
